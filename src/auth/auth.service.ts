b03519cbef4e1808b0dd77aa454571271422dfc1
jiUmin <wjdwkjh94@gmail.com>
2023-02-26 03:06:05 +0900

    user insert

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(UserRepository)
+    @InjectRepository(User)
     private userRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    //return this.userRepository.createUser(authCredentialsDto); // 위에 board때와 같은 에러, 임시로 user.repository로 뺏던 로직을 auth.service에서 다시 구현
+    const { username, password } = authCredentialsDto;
+    const user = this.userRepository.create({ username, password });
+    await this.userRepository.save(user);
+  }
 }
