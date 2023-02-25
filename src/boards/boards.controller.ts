0ec6bf2b52ea7e9aeecf70a0dd5adfc1eadf3cee
jiUmin <wjdwkjh94@gmail.com>
2023-02-26 02:06:00 +0900

    db select, delete, insert

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, ValidationPipe } from '@nestjs/common';
 import {
   Body,
   Delete,
@@ -7,7 +7,8 @@ import {
   Post,
   UsePipes,
 } from '@nestjs/common/decorators';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -15,32 +16,46 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
-  //@Get('/')
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoards();
-  }
+  ////@Get('/')
+  // @Get()
+  // getAllBoard(): Board[] {
+  //   return this.boardsService.getAllBoards();
+  // }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   return this.boardsService.getBoardById(id);
+  // }
+
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardsService.createBoard(createBoardDto);
   }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
 
   @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardsService.deleteBoard(id);
+  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.boardsService.deleteBoard(id);
   }
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') id: string): void {
+  //   this.boardsService.deleteBoard(id);
+  // }
 
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ) {
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ) {
+  //   return this.boardsService.updateBoardStatus(id, status);
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,47 +1,80 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid'; //v1 버젼 쓴다 (uuid란 이름으로 쓰겠다)
 import { CreateBoardDto } from './dto/create-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
-
-  getAllBoards(): Board[] {
-    return this.boards;
-  }
-
-  getBoardById(id: string): Board {
-    const found = this.boards.find((board) => board.id === id);
+  constructor(
+    @InjectRepository(Board) //https://www.inflearn.com/questions/651328/typeorm-db-%EC%97%B0%EA%B2%B0-%EC%98%A4%EB%A5%98%EC%9E%85%EB%8B%88%EB%8B%A4
+    private boardRepository: BoardRepository,
+  ) {}
+  // getAllBoards(): Board[] {
+  //   return this.boards;
+  // }
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
+    //const found = await this.boardRepository.findOne({ where: { id } }); //$ npm install typeorm@0.2  명령어를 통해서 공식문서에서 권장하는 버전으로 변경 부탁드립니다.
     if (!found) {
       throw new NotFoundException(`Can't find Board with id ${id}`);
     }
 
     return found;
   }
+  // getBoardById(id: string): Board {
+  //   const found = this.boards.find((board) => board.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Can't find Board with id ${id}`);
+  //   }
+  //   return found;
+  // }
 
-  createBoard(createBoardDto: CreateBoardDto) {
-    // const title = createBoardDto.title;
-    // const {title} = createBoardDto;
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    //return this.boardRepository.createBoard(createBoardDto); //typeorm 0.3에서부터는 deprecated 되어 동작하지 않는다고 합니다. 데이터 베이스 관련 로직을 레퍼지토리로 옮기지 마시고 서비스에서 처리하면 해결될 듯 합니다.
+    //https://velog.io/@wonjun1995/NestJS9.x.x-TypeORM0.3.x%EC%97%90%EC%84%9C-customRepository-%EC%89%BD%EA%B2%8C-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
     const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title, // == title: title,
-      description, // == description: description,
+
+    const board = this.boardRepository.create({
+      title,
+      description,
       status: BoardStatus.PUBLIC,
-    };
-    this.boards.push(board);
+    });
+
+    await this.boardRepository.save(board);
     return board;
   }
 
-  deleteBoard(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== found.id);
-  }
+  // createBoard(createBoardDto: CreateBoardDto) {
+  //   // const title = createBoardDto.title;
+  //   // const {title} = createBoardDto;
+  //   const { title, description } = createBoardDto;
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title, // == title: title,
+  //     description, // == description: description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.delete(id);
 
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
+    //지우려는 자료가 없을때
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
   }
+  // deleteBoard(id: string): void {
+  //   const found = this.getBoardById(id);
+  //   this.boards = this.boards.filter((board) => board.id !== found.id);
+  // }
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   const board = this.getBoardById(id);
+  //   board.status = status;
+  //   return board;
+  // }
 }
