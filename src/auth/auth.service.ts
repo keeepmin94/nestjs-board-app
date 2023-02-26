01d3e0376ad5f9200215db1cc96f99dfb9d4e590
jiUmin <wjdwkjh94@gmail.com>
2023-02-26 21:37:02 +0900

    user unique id

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
@@ -15,6 +19,15 @@ export class AuthService {
     //return this.userRepository.createUser(authCredentialsDto); // 위에 board때와 같은 에러, 임시로 user.repository로 뺏던 로직을 auth.service에서 다시 구현
     const { username, password } = authCredentialsDto;
     const user = this.userRepository.create({ username, password });
-    await this.userRepository.save(user);
+
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
